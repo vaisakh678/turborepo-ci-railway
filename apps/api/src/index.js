cef2bbcfde4444822f2643eeb236f75c20095dc1
vaisakh b <vaisakh@vaisakhs-MacBook-Pro.local>
2025-10-21 19:45:22 +0530

    module type

--- apps/api/src/index.js
+++ apps/api/src/index.js
@@ -1,12 +1,11 @@
 import express from "express";
-
 const app = express();
+const port = 3000;
 
 app.get("/", (req, res) => {
-	res.send("Hello, World!");
+	res.send("Hello World!");
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
 });
